e59b062eb1276b6fcd51c867ecd8843436ee7a70
Priyanshu Sahu <priyanshusahu5726@gmail.com>
2026-09-21 21:25:43 +0530

    Add formatted product price

--- Sources/QuickCartDomain/Product.swift
+++ Sources/QuickCartDomain/Product.swift
@@ -5,6 +5,9 @@
 //  Created by priyanshu sahu on 18/09/26.
 //
 // QuickCartDomain 1.0.1
+
+import Foundation
+
 public struct Product: Identifiable, Sendable {
     public let id: Int
     public let name: String
@@ -27,5 +30,8 @@ public struct Product: Identifiable, Sendable {
         self.image = image
         self.category = category
         self.description = description
+    }
+    public var formattedPrice: String {
+        String(format: "$%.2f", price)
     }
 }
